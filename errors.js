27647e28239be47bfcc82612a5d0cc49e7febca6
Kyle Stevenson <ky_steve@hotmail.com>
2017-03-25 10:16:38 -0400

    updates to test and adaptor

---
+++ errors.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Kyle on 2017-03-22.
+ */
+module.exports = {
+    MALFORMED_JSON:1
+    ,ONLY_JSON_OBJECTS:2
+    ,MISSING_BODY:3
+    ,MISSING_FIELD:4
+    ,BAD_DATA_TYPE:5
+    ,COULD_NOT_CONNECT:13
+    ,COULD_NOT_START_DEVICE:14
+};
